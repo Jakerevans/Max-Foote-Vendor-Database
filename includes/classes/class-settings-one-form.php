ba3fcbb7fe572ac866234c641d4e04e710558ad2
5HAN7ANU <5HAN7ANU@GMAIL.COM>
2020-04-02 15:20:40 -0500

    Vendor html
    Vendor html for the settings page

--- includes/classes/class-settings-one-form.php
+++ includes/classes/class-settings-one-form.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Maxfoote Book Display Options Form Tab Class - class-maxfoote-book-display-options-form.php
  *
@@ -8,30 +9,31 @@
  * @version  6.1.5.
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
-if ( ! class_exists( 'Maxfoote_Settings1_Form', false ) ) :
+if (!class_exists('Maxfoote_Settings1_Form', false)) :
 
 	/**
 	 * Maxfoote_Admin_Menu Class.
 	 */
-	class Maxfoote_Settings1_Form {
+	class Maxfoote_Settings1_Form
+	{
 
 
 		/**
 		 * Class Constructor - Simply calls the Translations
 		 */
-		public function __construct() {
-
-
+		public function __construct()
+		{
 		}
 
 		/**
 		 * Outputs all HTML elements on the page.
 		 */
-		public function output_settings1_form() {
+		public function output_settings1_form()
+		{
 			global $wpdb;
 
 			/*
@@ -240,6 +242,101 @@ if ( ! class_exists( 'Maxfoote_Settings1_Form', false ) ) :
 						</div>
 					</div>
 				</div>
+				<div class="maxfoote-form-section-wrapper">
+					<div class="maxfoote-form-section-title-wrapper">
+						<p class="maxfoote-form-subtitle">Vendor Information</p>
+					</div>
+					<div class="maxfoote-form-section-fields-wrapper">
+						<div class="maxfoote-form-section-fields-indiv-wrapper">
+							<label class="maxfoote-form-section-fields-label">Vendor Name</label>
+							<input class="maxfoote-form-section-fields-input maxfoote-form-section-fields-input-text"
+								id="maxfoote-form-newsite-vendorname" data-dbname="vendorname" type="text" />
+						</div>
+						<div class="maxfoote-form-section-fields-indiv-wrapper">
+							<label class="maxfoote-form-section-fields-label">Vendor Type</label>
+							<input class="maxfoote-form-section-fields-input maxfoote-form-section-fields-input-text"
+								id="maxfoote-form-newsite-vendortype" data-dbname="vendortype" type="text" />
+						</div>
+						<div class="maxfoote-form-section-fields-indiv-wrapper">
+							<label class="maxfoote-form-section-fields-label">Vendor Certfications</label>
+							<input class="maxfoote-form-section-fields-input maxfoote-form-section-fields-input-text"
+								id="maxfoote-form-newsite-vendorcerts" data-dbname="vendorcerts" type="text" />
+						</div>
+						<div class="maxfoote-form-section-fields-indiv-wrapper">
+							<label class="maxfoote-form-section-fields-label">Vendor License</label>
+							<input class="maxfoote-form-section-fields-input maxfoote-form-section-fields-input-text"
+								id="maxfoote-form-newsite-vendorlicense" data-dbname="vendorlicense" type="text" />
+						</div>
+					</div>
+					<div class="maxfoote-form-section-fields-wrapper">
+						<div class="maxfoote-form-section-fields-indiv-wrapper">
+							<label class="maxfoote-form-section-fields-label">Vendor Trade</label>
+							<input class="maxfoote-form-section-fields-input maxfoote-form-section-fields-input-text"
+								id="maxfoote-form-newsite-vendortrade" data-dbname="vendortrade" type="text" />
+						</div>
+						<div class="maxfoote-form-section-fields-indiv-wrapper">
+							<label class="maxfoote-form-section-fields-label">Vendor Address Line 1</label>
+							<input class="maxfoote-form-section-fields-input maxfoote-form-section-fields-input-text"
+								id="maxfoote-form-newsite-vendoraddress" data-dbname="vendoraddress" type="text" />
+						</div>
+						<div class="maxfoote-form-section-fields-indiv-wrapper">
+							<label class="maxfoote-form-section-fields-label">Vendor Address Line 2</label>
+							<input class="maxfoote-form-section-fields-input maxfoote-form-section-fields-input-text"
+								id="maxfoote-form-newsite-vendoraddress2" data-dbname="vendoraddress2" type="text" />
+						</div>
+					</div>
+					<div class="maxfoote-form-section-fields-wrapper">
+						<div class="maxfoote-form-section-fields-indiv-wrapper">
+							<label class="maxfoote-form-section-fields-label">Vendor City</label>
+							<input class="maxfoote-form-section-fields-input maxfoote-form-section-fields-input-text"
+								id="maxfoote-form-newsite-vendorcity" data-dbname="vendorcity" type="text" />
+						</div>
+						<div class="maxfoote-form-section-fields-indiv-wrapper">
+							<label class="maxfoote-form-section-fields-label">Vendor State</label>
+							<input class="maxfoote-form-section-fields-input maxfoote-form-section-fields-input-text"
+								id="maxfoote-form-newsite-vendorstate" data-dbname="vendorstate" type="text" />
+						</div>
+						<div class="maxfoote-form-section-fields-indiv-wrapper">
+							<label class="maxfoote-form-section-fields-label">Vendor Zip</label>
+							<input class="maxfoote-form-section-fields-input maxfoote-form-section-fields-input-text"
+								id="maxfoote-form-newsite-vendorzip" data-dbname="vendorzip" type="text" />
+						</div>
+					</div>
+					<div class="maxfoote-form-section-fields-wrapper">
+						<div class="maxfoote-form-section-fields-indiv-wrapper">
+							<label class="maxfoote-form-section-fields-label">Vendor Phone</label>
+							<input class="maxfoote-form-section-fields-input maxfoote-form-section-fields-input-text"
+								id="maxfoote-form-newsite-vendorphone" data-dbname="vendorphone" type="text" />
+						</div>
+						<div class="maxfoote-form-section-fields-indiv-wrapper">
+							<label class="maxfoote-form-section-fields-label">Vendor Contact</label>
+							<input class="maxfoote-form-section-fields-input maxfoote-form-section-fields-input-text"
+								id="maxfoote-form-newsite-vendorcontact" data-dbname="vendorcontact" type="text" />
+						</div>
+						<div class="maxfoote-form-section-fields-indiv-wrapper">
+							<label class="maxfoote-form-section-fields-label">Vendor Email</label>
+							<input class="maxfoote-form-section-fields-input maxfoote-form-section-fields-input-text"
+								id="maxfoote-form-newsite-vendoremail" data-dbname="vendoremail" type="text" />
+						</div>
+					</div>
+					<div class="maxfoote-form-section-fields-wrapper">
+						<div class="maxfoote-form-section-fields-indiv-wrapper">
+							<label class="maxfoote-form-section-fields-label">Vendor Enterprise</label>
+							<input class="maxfoote-form-section-fields-input maxfoote-form-section-fields-input-text"
+								id="maxfoote-form-newsite-vendorenterprise" data-dbname="vendorenterprise" type="text" />
+						</div>
+						<div class="maxfoote-form-section-fields-indiv-wrapper">
+							<label class="maxfoote-form-section-fields-label">Vendor Last Updated</label>
+							<input class="maxfoote-form-section-fields-input maxfoote-form-section-fields-input-text"
+								id="maxfoote-form-newsite-vendorlastupdated" data-dbname="vendorlastupdated" type="date" />
+						</div>
+						<div class="maxfoote-form-section-fields-indiv-wrapper">
+							<label class="maxfoote-form-section-fields-label">Event Location</label>
+							<input class="maxfoote-form-section-fields-input maxfoote-form-section-fields-input-text"
+								id="maxfoote-form-newsite-eventlocation" data-dbname="eventlocation" type="text" />
+						</div>
+					</div>
+				</div>	
 				<div class="maxfoote-form-section-wrapper">
 					<div class="maxfoote-form-section-title-wrapper">
 							<p class="maxfoote-form-subtitle">Technical Information</p>
